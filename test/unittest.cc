6cda9ba03ca5f6507ed6f4754db0a3dbf23822b9
vNaonLu <pingu326326@gmail.com>
2021-09-09 20:01:08 +0800

    adds question #36

--- test/unittest.cc
+++ test/unittest.cc
@@ -4,6 +4,7 @@
 
 #include "q0001_unittest.hpp"
 #include "q0019_unittest.hpp"
+#include "q0036_unittest.hpp"
 #include "q0088_unittest.hpp"
 #include "q0118_unittest.hpp"
 #include "q0121_unittest.hpp"
