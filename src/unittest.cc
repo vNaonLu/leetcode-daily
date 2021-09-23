8245f92f7908deda8a4ac1d1593ed117b406baf2
vNaonLu <pingu326326@gmail.com>
2021-09-23 15:25:29 +0800

    adds question #1328

--- src/unittest.cc
+++ src/unittest.cc
@@ -23,6 +23,7 @@
 #include "q_1251_1300/unittest.hpp"
 #include "q_451_500/unittest.hpp"
 #include "q_1201_1250/unittest.hpp"
+#include "q_1301_1350/unittest.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
