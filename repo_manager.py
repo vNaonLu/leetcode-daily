7289a426148673a03055f7dc91b7b6aa35c1591a
vNaonLu <pingu326326@gmail.com>
2021-09-24 23:23:01 +0800

    adds delete feature with the repo_manager

--- repo_manager.py
+++ repo_manager.py
@@ -12,26 +12,32 @@ def __parser():
                       action="store_true",
                       default=False,
                       help="feature to add question and modify README and src automatically.")
-    # parser.add_option("-d", "--del",
-    #                   dest="del_identify",
-    #                   action="store_true",
-    #                   default=False,
-    #                   help="feature to delete question and modify README and src automatically.")
+    parser.add_option("-d", "--del",
+                      dest="del_identify",
+                      action="store_true",
+                      default=False,
+                      help="feature to delete question and modify README and src automatically.")
     return parser
 
 
 def __main():
     parser = __parser()
     options, args = parser.parse_args()
-    # if options.add_identify and options.del_identify:
-    #     parser.error("options -a and -d are mutually exclusive.")
-    file_path = pathlib.Path(__file__).parent.resolve()
+    if options.add_identify and options.del_identify:
+        parser.error("options -a and -d are mutually exclusive.")
+    file_path = pathlib.Path(__file__).parent
     if options.add_identify:
-        subprocess.run(["python3", os.path.join(file_path, "./tools/leetcode_add.py"),
-                        "--out", os.path.join(file_path, "./src/"),
-                        "--readme", os.path.join(file_path, "./README.md"),
-                        "--question-list", os.path.join(file_path, "./src/questions_list.csv"),
-                        "--question-log", os.path.join(file_path, "./src/logs.csv")] + args)
+        subprocess.run(["python3", file_path.joinpath("./tools/leetcode_add.py").resolve(),
+                        "--out", file_path.joinpath("./src/").resolve(),
+                        "--readme", file_path.joinpath("./README.md").resolve(),
+                        "--question-list", file_path.joinpath("./src/questions_list.csv").resolve(),
+                        "--question-log", file_path.joinpath("./src/logs.csv").resolve()] + args)
+    if options.del_identify:
+        subprocess.run(["python3", file_path.joinpath("./tools/leetcode_del.py").resolve(),
+                        "--out", file_path.joinpath("./src/").resolve(),
+                        "--readme", file_path.joinpath("./README.md").resolve(),
+                        "--question-list", file_path.joinpath("./src/questions_list.csv").resolve(),
+                        "--question-log", file_path.joinpath("./src/logs.csv").resolve()] + args)
 
 
 if __name__ == "__main__":
