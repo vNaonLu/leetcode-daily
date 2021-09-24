6bf82762d2efe11488247a1056f88afa2c6e30c5
vNaonLu <pingu326326@gmail.com>
2021-09-25 00:27:49 +0800

    sorts unittest

--- src/q_101_150/unittest.hpp
+++ src/q_101_150/unittest.hpp
@@ -1,18 +1,20 @@
 
 #ifndef Q_101_150_UNITTEST
 #define Q_101_150_UNITTEST
+
+#include "q0101_unittest.hpp"
+#include "q0102_unittest.hpp"
+#include "q0104_unittest.hpp"
+#include "q0112_unittest.hpp"
+#include "q0115_unittest.hpp"
 #include "q0116_unittest.hpp"
 #include "q0118_unittest.hpp"
+#include "q0119_unittest.hpp"
+#include "q0120_unittest.hpp"
 #include "q0121_unittest.hpp"
+#include "q0136_unittest.hpp"
 #include "q0141_unittest.hpp"
 #include "q0144_unittest.hpp"
 #include "q0145_unittest.hpp"
-#include "q0102_unittest.hpp"
-#include "q0104_unittest.hpp"
-#include "q0101_unittest.hpp"
-#include "q0120_unittest.hpp"
-#include "q0112_unittest.hpp"
-#include "q0136_unittest.hpp"
-#include "q0115_unittest.hpp"
-#include "q0119_unittest.hpp"
+
 #endif
