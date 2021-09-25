d23ca8bc7fe2d077bf26fd1e2db84261c76f16df
vNaonLu <pingu326326@gmail.com>
2021-09-25 16:38:39 +0800

    adds question #290

--- src/q_251_300/unittest.hpp
+++ src/q_251_300/unittest.hpp
@@ -1,6 +1,9 @@
 
 #ifndef Q_251_300_UNITTEST
 #define Q_251_300_UNITTEST
-#include "q0283_unittest.hpp"
+
 #include "q0282_unittest.hpp"
+#include "q0283_unittest.hpp"
+#include "q0290_unittest.hpp"
+
 #endif
