a313f16cf969340e052fc51ff5d79a1a44af50c6
vNaonLu <pingu326326@gmail.com>
2021-09-26 17:50:46 +0800

    adds question #49

--- src/q_1_50/unittest.hpp
+++ src/q_1_50/unittest.hpp
@@ -1,16 +1,19 @@
 
 #ifndef Q_1_50_UNITTEST
 #define Q_1_50_UNITTEST
+
 #include "q0001_unittest.hpp"
 #include "q0003_unittest.hpp"
+#include "q0011_unittest.hpp"
+#include "q0015_unittest.hpp"
 #include "q0019_unittest.hpp"
 #include "q0020_unittest.hpp"
 #include "q0021_unittest.hpp"
+#include "q0033_unittest.hpp"
+#include "q0034_unittest.hpp"
 #include "q0036_unittest.hpp"
 #include "q0046_unittest.hpp"
-#include "q0015_unittest.hpp"
-#include "q0034_unittest.hpp"
-#include "q0033_unittest.hpp"
 #include "q0048_unittest.hpp"
-#include "q0011_unittest.hpp"
+#include "q0049_unittest.hpp"
+
 #endif
