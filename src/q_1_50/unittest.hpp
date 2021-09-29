1571c07d400cb0e05f99698342bf9da7d0823acc
vNaonLu <pingu326326@gmail.com>
2021-09-29 13:19:48 +0800

    adds question 22

--- src/q_1_50/unittest.hpp
+++ src/q_1_50/unittest.hpp
@@ -12,6 +12,7 @@
 #include "q0019_unittest.hpp"
 #include "q0020_unittest.hpp"
 #include "q0021_unittest.hpp"
+#include "q0022_unittest.hpp"
 #include "q0033_unittest.hpp"
 #include "q0034_unittest.hpp"
 #include "q0036_unittest.hpp"
