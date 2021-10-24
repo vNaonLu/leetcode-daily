bf2aa6ad2c9fcb0f9d0bb791cdab85084f52247b
vNaonLu <pingu326326@gmail.com>
2021-10-24 22:07:25 +0800

    improves the readability of question id 62

--- src/q_51_100/q0062.hpp
+++ src/q_51_100/q0062.hpp
@@ -35,19 +35,13 @@ class Solution {
  public:
   int uniquePaths(int m, int n) {
     vector<vector<int>> dp(m, vector<int>(n, 0));
-    for (int i = 0; i < m; ++i) {
-      for (int j = 0; j < n; ++j) {
-        if (i == 0 && j == 0) {
-          dp[i][j] = 1;
-        } else if (j == 0) {
-          dp[i][j] = dp[i - 1][j];
-        } else if (i == 0) {
-          dp[i][j] = dp[i][j - 1];
-        } else {
-          dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        }
-      }
-    }
+    for (int i = 0; i < m; ++i)
+      dp[i][0] = 1;
+    for (int i = 0; i < n; ++i)
+      dp[0][i] = 1;
+    for (int i = 1; i < m; ++i)
+      for (int j = 1; j < n; ++j)
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
     return dp.back().back();
   }
 };
