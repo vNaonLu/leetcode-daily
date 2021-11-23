2091bcbf4f0077df609c6b45576f0f703bcbcd4f
vNaonLu <pingu326326@gmail.com>
2021-11-23 18:55:09 +0800

    improves the question id 258 with TC O(1)

--- src/q_251_300/q0258.hpp
+++ src/q_251_300/q0258.hpp
@@ -26,15 +26,7 @@ using namespace std;
 class Solution {
  public:
   int addDigits(int num) {
-    while (num / 10 >= 1) {
-      int x = 0;
-      while (num != 0) {
-        x += num % 10;
-        num /= 10;
-      }
-      num = x;
-    }
-    return num;
+    return num == 0 ? 0 : 1 + (num - 1) % 9;
   }
 };
 }  // namespace l258
