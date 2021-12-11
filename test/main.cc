3d9837fe542dd55924a3411bce6729824f50c7fd
vNaonLu <pingu326326@gmail.com>
2021-12-11 20:21:01 +0800

    sets treenode test

--- test/main.cc
+++ test/main.cc
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 #include <leetcode/listnode.hpp>
+#include <leetcode/treenode.hpp>
 
 vector<vector<ListNode>> ListNode::keep_ = {};
+vector<vector<TreeNode>> TreeNode::keep_ = {};
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
