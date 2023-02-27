ce876721b0e8291aa37cfb5992a0975ba91f3a84
vNaonLu <pingu326326@gmail.com>
2023-02-28 00:22:57 +0800

    moves the old solutions for migrations.

--- src/main.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <leetcode/listnode.hpp>
-#include <leetcode/treenode.hpp>
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
