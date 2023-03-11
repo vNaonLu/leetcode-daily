8e0f47ba0768d5f86a614cc4d3afa817c2eb5d4b
vNaonLu <pingu326326@gmail.com>
2023-03-12 00:49:23 +0800

    adds timeout feature in cpp unittest infrastructure.

--- src/leetcode/testing/solution_test_helper.h
+++ src/leetcode/testing/solution_test_helper.h
@@ -3,11 +3,24 @@
 
 #include "leetcode/testing/test_helper.h"
 #include "gtest/gtest.h"
+#include <chrono>
+#include <functional>
+#include <future>
 #include <memory>
+#include <thread>
 
 namespace lcd {
 
 namespace detail {
+
+class SolutionDelegate {
+public:
+  virtual void Run() = 0;
+  int          operator()() {
+    Run();
+    return 0;
+  }
+};
 
 class SolutionFixture : public ::testing::Test {
 protected:
@@ -46,20 +59,52 @@ private:
 
 #define _LEETCODE_UNITTEST_FIXTURE_NAME(id, name) q##id##_##name
 
+#define _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)        \
+  q##id##_##name_##suite_name##_delegate
+
 #define LEETCODE_BEGIN_RESOLVING(id, name, solve_class)                        \
   class _LEETCODE_UNITTEST_FIXTURE_NAME(id, name)                              \
       : public ::lcd::detail::SolutionFixture {                                \
-  protected:                                                                   \
-    class solve_class
+  public:                                                                      \
+    class __Delegate;                                                          \
+                                                                               \
+    class __Delegate : public ::lcd::detail::SolutionDelegate {                \
+    protected:                                                                 \
+      class solve_class;                                                       \
+      template <typename... ArgsType>                                          \
+      std::unique_ptr<solve_class> Make##solve_class(ArgsType &&...args) {     \
+        return std::make_unique<solve_class>(std::forward<ArgsType>(args)...); \
+      }
 
 #define LEETCODE_END_RESOLVING(solve_class)                                    \
-  template <typename... ArgsType>                                              \
-  std::unique_ptr<solve_class> Make##solve_class(ArgsType &&...args) {         \
-    return std::make_unique<solve_class>(std::forward<ArgsType>(args)...);     \
   }                                                                            \
+  ;                                                                            \
   }
 
+#define LEETCODE_SOLUTION_UNITTEST_WITH_TIMED_OUT(id, name, suite_name, time)  \
+  class _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)        \
+      : public _LEETCODE_UNITTEST_FIXTURE_NAME(id, name)::__Delegate {         \
+  protected:                                                                   \
+    void Run() override;                                                       \
+  };                                                                           \
+  TEST_F(_LEETCODE_UNITTEST_FIXTURE_NAME(id, name), suite_name) {              \
+    auto delegate =                                                            \
+        std::make_unique<_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(            \
+            id, name, suite_name)>();                                          \
+    std::future<int> timeout =                                                 \
+        std::async(std::launch::async,                                         \
+                   &_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(                 \
+                       id, name, suite_name)::operator(),                      \
+                   *delegate);                                                 \
+    if (timeout.wait_for(std::chrono::milliseconds(time)) !=                   \
+        std::future_status::ready) {                                           \
+      FAIL() << "q" << #id << "_" << #name << "." << #suite_name               \
+             << " exceeded the time limit: " << #time << "ms";                 \
+    }                                                                          \
+  }                                                                            \
+  void _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)::Run()
+
 #define LEETCODE_SOLUTION_UNITTEST(id, name, suite_name)                       \
-  TEST_F(_LEETCODE_UNITTEST_FIXTURE_NAME(id, name), suite_name)
+  LEETCODE_SOLUTION_UNITTEST_WITH_TIMED_OUT(id, name, suite_name, 1000)
 
 #endif // TESTING_SOLUTION_HELPER_H_
