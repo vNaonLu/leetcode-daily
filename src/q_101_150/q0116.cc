3e94eb2dc9b0680c36019a332fb329c32c3b3791
vNaonLu <pingu326326@gmail.com>
2024-03-15 23:04:03 +0800

    adds unittests for q116.

--- src/q_101_150/q0116.cc
+++ src/q_101_150/q0116.cc
@@ -17,6 +17,20 @@
 using namespace std;
 using namespace lcd;
 
+namespace {
+class Node {
+public:
+  int   val;
+  Node *left;
+  Node *right;
+  Node *next;
+  Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+  Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+  Node(int _val, Node *_left, Node *_right, Node *_next)
+      : val(_val), left(_left), right(_right), next(_next) {}
+};
+} // namespace
+
 // Description of |116. Populating Next Right Pointers in Each Node|:
 //
 // You are given a perfect binary tree where all leaves are on the same level,
@@ -34,17 +48,6 @@ using namespace lcd;
 
 LEETCODE_BEGIN_RESOLVING(116, PopulatingNextRightPointersInEachNode, Solution);
 
-class Node {
-public:
-  int   val;
-  Node *left;
-  Node *right;
-  Node *next;
-  Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-  Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-  Node(int _val, Node *_left, Node *_right, Node *_next)
-      : val(_val), left(_left), right(_right), next(_next) {}
-};
 class Solution {
 public:
   Node *connect(Node *root) {
@@ -96,7 +99,26 @@ LEETCODE_END_RESOLVING(Solution);
 
 LEETCODE_SOLUTION_UNITTEST(116, PopulatingNextRightPointersInEachNode,
                            example_1) {
-  GTEST_SKIP() << "Unittest Not Implemented";
+  auto solution = MakeSolution();
+  Node n4(4);
+  Node n5(5);
+  Node n6(6);
+  Node n7(7);
+
+  Node  n2(2, &n4, &n5, nullptr);
+  Node  n3(3, &n6, &n7, nullptr);
+  Node  root(1, &n2, &n3, nullptr);
+  Node *actual = solution->connect(&root);
+  ASSERT_EQ(actual, &root);
+  ASSERT_EQ(actual->left, &n2);
+  ASSERT_EQ(actual->right, &n3);
+  ASSERT_EQ(actual->next, nullptr);
+  ASSERT_EQ(n2.next, &n3);
+  ASSERT_EQ(n3.next, nullptr);
+  ASSERT_EQ(n4.next, &n5);
+  ASSERT_EQ(n5.next, &n6);
+  ASSERT_EQ(n6.next, &n7);
+  ASSERT_EQ(n7.next, nullptr);
 }
 
 // [Example #2]
@@ -106,5 +128,9 @@ LEETCODE_SOLUTION_UNITTEST(116, PopulatingNextRightPointersInEachNode,
 
 LEETCODE_SOLUTION_UNITTEST(116, PopulatingNextRightPointersInEachNode,
                            example_2) {
-  GTEST_SKIP() << "Unittest Not Implemented";
+  auto  solution = MakeSolution();
+  Node *root     = nullptr;
+  Node *expect   = nullptr;
+  Node *actual   = solution->connect(root);
+  EXPECT_EQ(actual, expect);
 }
