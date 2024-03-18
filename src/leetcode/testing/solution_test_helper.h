5eb80abafc5258ee4ff831aa46ffa6b61e09e780
vNaonLu <pingu326326@gmail.com>
2024-03-19 00:19:48 +0800

    Add namespace for each problem.

--- src/leetcode/testing/solution_test_helper.h
+++ src/leetcode/testing/solution_test_helper.h
@@ -37,47 +37,61 @@ private:
 #define _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)        \
   q##id##_##name_##suite_name##_delegate
 
+#define _LEETCODE_SOLUTION_NAMESPACE_NAME(id, name) q##id##name##Anon
+
+#define LEETCODE_SOLUTION_NAMESPACE(id, name)                                  \
+  namespace _LEETCODE_SOLUTION_NAMESPACE_NAME(id, name)
+
 #define LEETCODE_BEGIN_RESOLVING(id, name, solve_class)                        \
-  class _LEETCODE_UNITTEST_FIXTURE_NAME(id, name)                              \
-      : public ::lcd::detail::SolutionFixture {                                \
-  public:                                                                      \
-    class __Delegate;                                                          \
+  LEETCODE_SOLUTION_NAMESPACE(id, name) {                                      \
+    class _LEETCODE_UNITTEST_FIXTURE_NAME(id, name)                            \
+        : public ::lcd::detail::SolutionFixture {                              \
+    public:                                                                    \
+      class __Delegate;                                                        \
                                                                                \
-    class __Delegate : public ::lcd::detail::SolutionDelegate {                \
-    protected:                                                                 \
-      class solve_class;                                                       \
-      template <typename... ArgsType>                                          \
-      std::unique_ptr<solve_class> Make##solve_class(ArgsType &&...args) {     \
-        return std::make_unique<solve_class>(std::forward<ArgsType>(args)...); \
-      }
+      class __Delegate : public ::lcd::detail::SolutionDelegate {              \
+      protected:                                                               \
+        class solve_class;                                                     \
+        template <typename... ArgsType>                                        \
+        std::unique_ptr<solve_class> Make##solve_class(ArgsType &&...args) {   \
+          return std::make_unique<solve_class>(                                \
+              std::forward<ArgsType>(args)...);                                \
+        }
 
 #define LEETCODE_END_RESOLVING(solve_class)                                    \
+  }                                                                            \
+  ;                                                                            \
   }                                                                            \
   ;                                                                            \
   }
 
 #define LEETCODE_SOLUTION_UNITTEST_WITH_TIMED_OUT(id, name, suite_name, time)  \
-  class _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)        \
-      : public _LEETCODE_UNITTEST_FIXTURE_NAME(id, name)::__Delegate {         \
-  protected:                                                                   \
-    void Run() override;                                                       \
-  };                                                                           \
-  TEST_F(_LEETCODE_UNITTEST_FIXTURE_NAME(id, name), suite_name) {              \
-    auto delegate =                                                            \
-        std::make_unique<_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(            \
-            id, name, suite_name)>();                                          \
-    std::future<int> timeout =                                                 \
-        std::async(std::launch::async,                                         \
-                   &_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(                 \
-                       id, name, suite_name)::operator(),                      \
-                   *delegate);                                                 \
-    if (timeout.wait_for(std::chrono::milliseconds(time)) !=                   \
-        std::future_status::ready) {                                           \
-      FAIL() << "q" << #id << "_" << #name << "." << #suite_name               \
-             << " exceeded the time limit: " << #time << "ms";                 \
+  LEETCODE_SOLUTION_NAMESPACE(id, name) {                                      \
+    class _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)      \
+        : public _LEETCODE_UNITTEST_FIXTURE_NAME(id, name)::__Delegate {       \
+    protected:                                                                 \
+      void Run() override;                                                     \
+    };                                                                         \
+    TEST_F(_LEETCODE_UNITTEST_FIXTURE_NAME(id, name), suite_name) {            \
+      using LEETCODE_SOLUTION_NAMESPACE(id, name);                             \
+      auto delegate =                                                          \
+          std::make_unique<_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(          \
+              id, name, suite_name)>();                                        \
+      std::future<int> timeout =                                               \
+          std::async(std::launch::async,                                       \
+                     &_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(               \
+                         id, name, suite_name)::operator(),                    \
+                     *delegate);                                               \
+      if (timeout.wait_for(std::chrono::milliseconds(time)) !=                 \
+          std::future_status::ready) {                                         \
+        FAIL() << "q" << #id << "_" << #name << "." << #suite_name             \
+               << " exceeded the time limit: " << #time << "ms";               \
+      }                                                                        \
     }                                                                          \
   }                                                                            \
-  void _LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name, suite_name)::Run()
+  void _LEETCODE_SOLUTION_NAMESPACE_NAME(                                      \
+      id, name)::_LEETCODE_SOLUTION_UNITTEST_DELEGATE_NAME(id, name,           \
+                                                           suite_name)::Run()
 
 #define LEETCODE_SOLUTION_UNITTEST(id, name, suite_name)                       \
   LEETCODE_SOLUTION_UNITTEST_WITH_TIMED_OUT(id, name, suite_name, 4000)
